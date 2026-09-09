48ecf1c46c2baa35f2c453ffa5004a7a001c146c
DaniilTurpitko <turpitcasdaniil2008@gmail.com>
2026-09-09 20:05:01 +0500

    Task3

---
+++ Tyuiu.TurpitkoDR.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿
+using Tyuiu.TurpitkoDR.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.TurpitkoDR.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
