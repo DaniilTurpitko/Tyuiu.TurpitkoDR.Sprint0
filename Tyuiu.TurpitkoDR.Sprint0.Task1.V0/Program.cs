f2115f0d2e5e88e8d3c90f817ace606ff12dbb62
DaniilTurpitko <turpitcasdaniil2008@gmail.com>
2026-09-09 18:22:08 +0500

    Task1

---
+++ Tyuiu.TurpitkoDR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.TurpitkoDR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Даниил"; 
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня завут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
